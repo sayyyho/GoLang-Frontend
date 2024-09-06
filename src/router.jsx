c47bdd338ca977fada531298daf7bd12692eb062
sayyyho <323psh@naver.com>
2024-09-06 19:37:41 +0900

    Fix: 경로 에러 수정

--- src/router.jsx
+++ src/router.jsx
@@ -36,6 +36,7 @@ const router = createBrowserRouter([
       //   path: "chatting/peer/relation",
       //   element: <ChatRelation />,
       // },
+      {
         path: "mypage",
         element: <MyPage />,
       },
