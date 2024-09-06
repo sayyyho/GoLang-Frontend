5ea8609e9c665ef58b1c069bbfaa106204641d49
sayyyho <323psh@naver.com>
2024-09-06 22:50:47 +0900

    Fix: localStorage 에러 수정

--- src/api/getID.js
+++ src/api/getID.js
@@ -3,11 +3,11 @@ import { instance } from "./instance";
 export const getUserID = async () => {
   try {
     const res = await instance.post(`/api/login`, {});
-    localStorage.setItem("username", res.data.username);
+    if (res.data && res.data.username) {
+      localStorage.setItem("username", res.data.username);
+    }
     // 또 링크 받았을 때, 아예 온보딩으로 이동시킨 뒤에 발급받고 채팅 참가하도록
   } catch (err) {
     console.log(err);
   }
 };
-
-export const Testing = "";
