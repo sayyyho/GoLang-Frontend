b6cb919723735f16cfdce1875ccfdbb967cbff27
sayyyho <323psh@naver.com>
2024-09-06 19:34:58 +0900

    Feat: PDF Upload 페이지 구현

--- src/pages/ChatSetting/ChatInfo.jsx
+++ src/pages/ChatSetting/ChatInfo.jsx
@@ -1,8 +1,39 @@
 import * as S from "./style";
+import { useRef, useState } from "react";
 import { Header } from "@/components/Header/Header";
 import CHAT_LAYOUT from "@/assets/ChatLayout.png";
+import REL_GOLANG from "@/assets/relationGolang.svg";
+import { RELATION_TEXT } from "@/constant/chatSetting";
+import { postPdf } from "@/api/postPdf";
 
 export const ChatInfo = () => {
+  const fileInputRef = useRef();
+  const [isUpload, setIsUpload] = useState(false);
+  const [file, setFile] = useState(null);
+
+  const handleSubmit = async () => {
+    if (file) {
+      const res = await postPdf(file);
+      console.log(res);
+    } else {
+      alert("Please upload a PDF file.");
+    }
+  };
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile && selectedFile.type === "application/pdf") {
+      setIsUpload(true);
+      setFile(selectedFile);
+    }
+  };
+
+  const handleUploadClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
   return (
     <S.Layout
       style={{
@@ -14,7 +45,38 @@ export const ChatInfo = () => {
       <Header color="black" isBack={true}>
         2인 채팅
       </Header>
-      <S.SettingZone></S.SettingZone>
+      <S.SettingZone>
+        <img src={REL_GOLANG} />
+        {RELATION_TEXT.map((text, index) => (
+          <S.Text key={index}>{text}</S.Text>
+        ))}
+        <S.UploadZone>
+          <input
+            ref={fileInputRef}
+            type="file"
+            accept=".pdf"
+            style={{ display: "none" }}
+            onChange={handleFileChange}
+          />
+          {!isUpload ? (
+            <S.PlusBtn onClick={handleUploadClick} />
+          ) : (
+            <S.PdfView>
+              <S.PdfImage />
+              <p
+                style={{
+                  fontSize: "0.8rem",
+                }}
+              >
+                {file.name}
+              </p>
+            </S.PdfView>
+          )}
+        </S.UploadZone>
+        <S.CustomBtn onClick={handleSubmit}>
+          <S.Text color="white">Start Chatting</S.Text>
+        </S.CustomBtn>
+      </S.SettingZone>
     </S.Layout>
   );
 };
--- src/pages/ChatSetting/style.js
+++ src/pages/ChatSetting/style.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { FaPlusCircle, FaFilePdf } from "react-icons/fa";
 
 export const Layout = styled.div`
   min-height: 100vh;
@@ -12,4 +13,58 @@ export const Layout = styled.div`
 export const SettingZone = styled.div`
   width: 100%;
   min-height: 90%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const Text = styled.p`
+  color: ${(props) => props.color || "black"};
+  font-family: "EF_jejudoldam", sans-serif;
+`;
+
+export const UploadZone = styled.div`
+  margin-top: 1rem;
+  background-color: white;
+  width: 250px;
+  height: 250px;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PlusBtn = styled(FaPlusCircle)`
+  font-size: 3.5rem;
+  cursor: pointer;
+`;
+
+export const PdfImage = styled(FaFilePdf)`
+  font-size: 3.5rem;
+  cursor: pointer;
+`;
+
+export const PdfView = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 1.5rem;
+`;
+
+export const CustomBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 250px;
+  height: 50px;
+  border-radius: 25px;
+  background: rgba(21, 77, 186, 0.53);
+  margin: 2rem 0;
+  /* filter: blur(0.5px); */
 `;
