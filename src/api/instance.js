2b803f9220f2d718468663034fd8648862a178de
sayyyho <323psh@naver.com>
2024-09-06 17:50:33 +0900

    Fix: API 에러 수정

--- src/api/instance.js
+++ src/api/instance.js
@@ -3,4 +3,24 @@ import axios from "axios";
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_API,
   timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
+
+instance.interceptors.request.use(
+  (config) => config,
+  (error) => Promise.reject(error)
+);
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.code === "ECONNABORTED") {
+      console.log("Request timeout");
+      window.location.href = "/error";
+    }
+    return Promise.reject(error);
+  }
+);
