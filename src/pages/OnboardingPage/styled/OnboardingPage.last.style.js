df8a0d1b16aab02cb06900bed8fabb1e55a6f411
Kyumin-Chang <kyumin0529@naver.com>
2024-09-06 21:26:14 +0900

    feat: 온보딩 페이지 구현

---
+++ src/pages/OnboardingPage/styled/OnboardingPage.last.style.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import onboardingBackground from "../../../assets/img/onboardingBackground.png";
+import onboardingContinueButton from "../../../assets/img/onboardingStartButton.png";
+
+export const Frame = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: "EF_jejudoldam", sans-serif;
+`;
+
+export const Wrapper = styled.div`
+    margin: 0 auto;
+    width: 100%;
+    height: 100%;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 420px;
+    background-color: white;
+    background-image: url(${onboardingBackground});
+    background-size: cover;
+    background-position: center;
+    border-radius: 30px;
+    justify-content: flex-end;
+`;
+
+export const Text = styled.p`
+    font-size: 1.1rem;
+    color: #397D9A;
+    line-height: 1.5;
+    margin-bottom: 200px;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+export const WhaleImage = styled.img`
+    width: 420px;
+    height: auto;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 30px;
+`;
+
+export const ContinueButton = styled.button`
+    width: 337px;
+    height: 59px;
+    max-width: 337px;
+    margin-bottom: 30px;
+    background-image: url(${onboardingContinueButton});
+    background-size: cover;
+    cursor: pointer;
+`;
