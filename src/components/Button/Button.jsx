a5df8deb40b7bbc55acae5a04fd9a8104e768c55
sayyyho <323psh@naver.com>
2024-09-07 09:15:20 +0900

    Fix

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,22 @@
 import * as S from "./style";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 export const EndButton = ({ children }) => {
-  return <S.StyledButton>{children}</S.StyledButton>;
+  const handleFinish = () => {
+    Swal.fire({
+      title: "종료하시겠습니까?",
+      showCancelButton: true,
+      confirmButtonText: "예",
+      cancelButtonText: "아니오",
+      confirmButtonColor: "#429f50",
+      cancelButtonColor: "#d33",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        location.href = "/";
+      }
+    });
+  };
+
+  return <S.StyledButton onClick={handleFinish}>{children}</S.StyledButton>;
 };
---
+++ src/pages/BotSetting/BotRelation.jsx
@@ -0,0 +1,73 @@
+import * as S from "./style";
+import CHAT_LAYOUT from "@/assets/ChatLayout.png";
+import REL_GOLANG from "@/assets/relationGolang.svg";
+import { useState } from "react";
+import { CHOOSE_TEXT } from "@/constant/chatSetting";
+import { Header } from "@/components/Header/Header";
+import { postDetail } from "@/api/postDetail";
+import { useNavigate } from "react-router-dom";
+
+const relation = ["커플", "친구", "가족", "기타"];
+const sendRelation = ["COUPLE", "FRIEND", "FAMILY", "ETC"];
+
+export const BotRelation = () => {
+  const navigate = useNavigate();
+  const [selectedBox, setSelectedBox] = useState([false, false, false, false]);
+  const [selectedNum, setSelectedNum] = useState(null);
+  const [chatDetails, setChatDetails] = useState(""); // 상태 추가
+
+  const handleSelect = (index) => {
+    const box = [false, false, false, false];
+    box[index] = true;
+    setSelectedBox(box);
+    setSelectedNum(index);
+  };
+
+  const handleInputChange = (event) => {
+    setChatDetails(event.target.value); // 입력된 값을 상태에 저장
+  };
+
+  const handleSubmit = async () => {
+    navigate("/chatting/bot/1");
+  };
+
+  return (
+    <S.Layout
+      style={{
+        backgroundImage: `url(${CHAT_LAYOUT})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Header color="black" isBack={true}>
+        고랭과 채팅
+      </Header>
+
+      <img src={REL_GOLANG} />
+      {CHOOSE_TEXT.map((text, index) => (
+        <S.Text key={index}>{text}</S.Text>
+      ))}
+      <S.RelationLayout>
+        {relation.map((text, index) => (
+          <S.SelectBox
+            onClick={() => handleSelect(index)}
+            key={index}
+            $isCheckd={selectedBox[index]}
+          >
+            {text}
+          </S.SelectBox>
+        ))}
+      </S.RelationLayout>
+      <S.StyledInput
+        maxLength={300}
+        rows={7}
+        placeholder="상대와의 갈등 상황을 입력해주세요."
+        value={chatDetails}
+        onChange={handleInputChange} // 입력 핸들러 추가
+      />
+      <S.CustomBtn onClick={handleSubmit}>
+        <S.Text color="white">Start Chatting</S.Text>
+      </S.CustomBtn>
+    </S.Layout>
+  );
+};
